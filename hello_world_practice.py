8d89f9d0202bf131c3311a8130188c491cac3e38
kerileannmarshall <81601478+kerileannmarshall@users.noreply.github.com>
2023-05-18 12:33:29 -0500

    Add files via upload

---
+++ hello_world_practice.py
@@ -0,0 +1,46 @@
+#First chapter of exercises for Python coding.
+#Keri Marshall
+#May 18, 2023
+
+print("Hello Python World!")
+
+message = "Hello Python World!"
+print(message)
+
+laurel_birthday = "Happy Birthday Laurel!"
+print(laurel_birthday)
+
+emma_birthday = "Happy 11th Birthday Emma!"
+print(emma_birthday)
+
+name = "keri marshall"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+first_name = "keri"
+last_name = "marshall"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(full_name.title() + " is learning to code in Python")
+
+
+new_name = "Eric"
+personal_message = f"Hello {new_name}, would you like to learn some Python today?"
+print(personal_message)
+print(new_name.title())
+print(new_name.lower())
+print(new_name.upper())
+
+famous_person = "Albert Einstein"
+famous_quote = f"{famous_person} once said, 'A person who never made a mistake never tried anything new.'"
+print(famous_quote)
+
+famous_musician = " Steven Tyler "
+print(famous_musician.lstrip())
+print(famous_musician.strip())
+print(famous_musician.strip())
+
+
